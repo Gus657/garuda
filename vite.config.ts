155ee34e2d6e3454d772c8ddb1192bac5403f865
Gus657 <ggusperezg@gmail.com>
2022-11-29 13:47:32 -0600

    ssr

--- vite.config.ts
+++ vite.config.ts
@@ -8,19 +8,22 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'garuda',
-      // the proper extensions will be added
+      formats: ['es', 'cjs'],
       fileName: 'garuda'
     },
+    ssr: true,
+    minify: 'esbuild',
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      input: resolve(__dirname, 'src/index.ts'),
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           vue: 'Vue'
-        }
+        },
+        exports: 'named'
       }
     }
   }
